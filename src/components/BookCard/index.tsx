c9b13ab5d1292cf062294a0e0ff87a671c86c139
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-02-23 18:00:28 -0300

    feat: Loading ratings in home page

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -6,28 +6,44 @@ import {
   Text,
 } from './styles'
 
-export const BookCard = () => {
+interface BookCardProps {
+  name: string
+  avatar_url: string
+  rating_date: string
+  rate?: number
+  book_cover_url: string
+  book_name: string
+  book_author: string
+  book_description: string
+}
+
+export const BookCard = ({
+  name,
+  avatar_url,
+  rating_date,
+  rate = null,
+  book_cover_url,
+  book_name,
+  book_author,
+  book_description,
+}: BookCardProps) => {
+  console.log(book_cover_url)
   return (
     <BookCardContainer>
       <AuthorSection>
-        <img src="" alt="" />
+        <img src={avatar_url} alt={name} />
         <div>
-          <span>Jaxson Dias</span>
-          <p>Hoje</p>
+          <span>{name}</span>
+          <p>{rating_date}</p>
         </div>
       </AuthorSection>
       <BookInfoSection>
-        <img src="" alt="" />
+        <img src={book_cover_url} alt={book_name} />
         <BookInfoContent>
-          <strong>O Hobbit</strong>
-          <span>J.R.R Tolkein</span>
+          <strong>{book_name}</strong>
+          <span>{book_author}</span>
 
-          <Text>
-            Semper et sapien proin vitae nisi. Feugiat neque integer donec et
-            aenean posuere amet ultrices. Cras fermentum id pulvinar varius leo
-            a in. Amet libero pharetra nunc elementum fringilla velit ipsum. Sed
-            vulputate massa velit nibh... ver mais
-          </Text>
+          <Text>{book_description}</Text>
         </BookInfoContent>
       </BookInfoSection>
     </BookCardContainer>
--- src/components/LatestRatings/index.tsx
+++ src/components/LatestRatings/index.tsx
@@ -3,15 +3,61 @@ import { BookCard } from '../BookCard'
 import { PageTitle } from '../ui/PageTitle'
 import { LastRatingContainer, LatestRatingsContainer } from './styles'
 import { LastReading } from '../LastReading'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '@/src/lib/axios'
+
+interface Rating {
+  id: string
+  name: string
+  author: string
+  summary: string
+  cover_url: string
+  ratings: Array<{
+    id: string
+    rate: number
+    created_at: string
+    user: {
+      id: string
+      name: string
+      avatar_url: string
+    }
+  }>
+}
 
 export const LatestRatings = () => {
+  const [ratings, setRatings] = useState<Rating[]>([])
+
+  const loadingRatings = useCallback(async () => {
+    const response = await api.get<Rating[]>('/books/ratings')
+
+    setRatings(response.data)
+  }, [])
+
+  useEffect(() => {
+    loadingRatings()
+  }, [loadingRatings])
+
   return (
     <LatestRatingsContainer>
       <PageTitle title="Início" icon={<ChartLineUp />} />
       <LastReading />
       <LastRatingContainer>
         <span>Avaliações mais recentes</span>
-        <BookCard />
+        {ratings.map((rating) => {
+          return (
+            <BookCard
+              key={rating.id}
+              name={rating.ratings[0].user.name}
+              avatar_url={rating.ratings[0].user.avatar_url}
+              rating_date={rating.ratings[0].created_at}
+              rate={rating.ratings[0].rate}
+              book_cover_url={rating.cover_url.replace('public', '')}
+              book_name={rating.name}
+              book_author={rating.author}
+              book_description={rating.summary}
+            />
+          )
+        })}
       </LastRatingContainer>
     </LatestRatingsContainer>
   )
