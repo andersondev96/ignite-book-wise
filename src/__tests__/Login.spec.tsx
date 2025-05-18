f1a8b6ad7ab22de333c1cb18f4a18923a0948e90
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-05-18 13:58:06 -0300

    tests: Configure Vitest

---
+++ src/__tests__/Login.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
+
+import Login from '../pages/login/index.page'
+
+vi.mock('next/router', () => ({
+  useRouter: () => ({
+    push: vi.fn(),
+  }),
+}))
+
+describe('Página de Login', () => {
+  it('Página de login está renderizando', () => {
+    render(<Login />)
+
+    expect(screen.getByText('Boas vindas!')).toBeInTheDocument()
+    expect(screen.getByText(/Faça seu login/i)).toBeInTheDocument()
+  })
+})
